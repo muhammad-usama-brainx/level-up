4c66928d85bcca86a819517acb537136cfe290f2
muhammad-usama-brainx <128455025+muhammad-usama-brainx@users.noreply.github.com>
2023-04-13 11:22:08 +0500

    bottom nav with empty fragments

--- app/src/main/java/com/example/levelup/LoginActivity.kt
+++ app/src/main/java/com/example/levelup/LoginActivity.kt
@@ -1,13 +1,25 @@
 package com.example.levelup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.levelup.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.hide()
+
+        binding.loginButton.setOnClickListener {
+            Intent(this, MainActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
