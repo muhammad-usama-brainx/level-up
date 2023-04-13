dc8e93ff16ecffcdec55a198db2aacb0aae9b34b
Usama Javed <usama@Usamas-MacBook-Air.local>
2023-04-13 06:36:13 +0500

    added viewBinidng

--- app/src/main/java/com/example/levelup/MainActivity.kt
+++ app/src/main/java/com/example/levelup/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.levelup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.view.LayoutInflater
+import com.example.levelup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
