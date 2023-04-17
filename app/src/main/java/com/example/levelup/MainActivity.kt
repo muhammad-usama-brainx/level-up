3ea6e10fe757402d2579141b570f0dafc59cca18
muhammad-usama-brainx <128455025+muhammad-usama-brainx@users.noreply.github.com>
2023-04-17 12:07:28 +0500

    added top border nav bar

--- app/src/main/java/com/example/levelup/MainActivity.kt
+++ app/src/main/java/com/example/levelup/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.levelup
 
+import SelectedNavItem
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.levelup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var selectedNavItem: SelectedNavItem
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -15,28 +19,56 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.hide()
 
+        selectedNavItem = SelectedNavItem.Home
+        updateActiveNavItem()
         replaceFragment(HomeFragment())
 
         binding.bottomNavigationBar.setOnItemSelectedListener {
 
-         when(it.itemId)
-         {
-             R.id.home -> replaceFragment(HomeFragment())
-             R.id.notification -> replaceFragment(NotificationFragment())
-             R.id.settings -> replaceFragment(SettingsFragment())
-         }
-          true
-     }
-
-
+            when (it.itemId) {
+                R.id.home -> {
+                    selectedNavItem = SelectedNavItem.Home
+                    updateActiveNavItem()
+                    replaceFragment(HomeFragment())
+                }
+                R.id.notification -> {
+                    selectedNavItem = SelectedNavItem.Notification
+                    updateActiveNavItem()
+                    replaceFragment(NotificationFragment())
+                }
+                R.id.settings -> {
+                    selectedNavItem = SelectedNavItem.Settings
+                    updateActiveNavItem()
+                    replaceFragment(SettingsFragment())
+                }
+            }
+            true
+        }
 
 
     }
 
-    private fun replaceFragment(fragment: Fragment)
-    {
+    private fun replaceFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.frameContainer,fragment)
+        transaction.replace(R.id.frameContainer, fragment)
         transaction.commit()
+    }
+
+    private fun updateActiveNavItem() {
+        binding.homeBorder.visibility = View.INVISIBLE
+        binding.notificationBorder.visibility = View.INVISIBLE
+        binding.settingsBorder.visibility = View.INVISIBLE
+
+        when (selectedNavItem) {
+            SelectedNavItem.Home -> {
+                binding.homeBorder.visibility = View.VISIBLE
+            }
+            SelectedNavItem.Notification -> {
+                binding.notificationBorder.visibility = View.VISIBLE
+            }
+            else -> {
+                binding.settingsBorder.visibility = View.VISIBLE
+            }
+        }
     }
 }
---
+++ app/src/main/java/utils.kt
@@ -0,0 +1,5 @@
+enum class SelectedNavItem{
+    Home,
+    Notification,
+    Settings
+}
