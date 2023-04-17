2c66b5aaa46ff593a4c730d82907795a8a790c98
muhammad-usama-brainx <128455025+muhammad-usama-brainx@users.noreply.github.com>
2023-04-17 13:50:26 +0500

    added logout button and viewmodel

--- app/src/main/java/com/example/levelup/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/levelup/fragments/SettingsFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.cardview.widget.CardView
+import androidx.lifecycle.ViewModelProvider
 import com.example.levelup.R
+import com.example.levelup.viewModels.SettingsFragmentViewModel
 
 class SettingsFragment : Fragment() {
 
@@ -16,6 +18,10 @@ class SettingsFragment : Fragment() {
     private lateinit var contactCardView: CardView
     private lateinit var privacyCardView: CardView
     private lateinit var faqCardView: CardView
+    private lateinit var logoutCardView: CardView
+
+    private lateinit var viewModel: SettingsFragmentViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,12 +33,20 @@ class SettingsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProvider(this)[SettingsFragmentViewModel::class.java]
+
         profileCardView = view.findViewById(R.id.profileCardView)
         changePasswordCardView = view.findViewById(R.id.changePasswordCardView)
         coachingCardView = view.findViewById(R.id.coachingPlanCardView)
         contactCardView = view.findViewById(R.id.contactUsCardView)
         privacyCardView = view.findViewById(R.id.privacyCardView)
         faqCardView = view.findViewById(R.id.faqCardView)
+        logoutCardView = view.findViewById(R.id.logoutCardView)
+
+        logoutCardView.setOnClickListener{
+            viewModel.onLogout()
+        }
+
 
     }
 
---
+++ app/src/main/java/com/example/levelup/viewModels/SettingsFragmentViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.levelup.viewModels
+
+import androidx.lifecycle.ViewModel
+
+class SettingsFragmentViewModel : ViewModel() {
+
+
+    fun onLogout() {
+        println("logout")
+    }
+}
